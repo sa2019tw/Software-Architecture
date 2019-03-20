97711f4dcafbc4c3e113713c2b04d49a24360f82
Jammy Tan <jammy1129@gmail.com>
2019-03-20 10:09:17 +0800

    hw1

---
+++ 107598025/src/service/CourseService.java
@@ -0,0 +1,40 @@
+package service;
+
+import DAO.ICourseDAO;
+import main.Course;
+
+import java.util.List;
+
+public class CourseService implements ICourseService {
+
+    private ICourseDAO courseDAO;
+
+    public CourseService(ICourseDAO courseDAO) {
+
+        this.courseDAO = courseDAO;
+    }
+
+    public void addCourse(Course course) {
+
+        courseDAO.addCourse(course);
+    }
+
+    public Course retrieveOneCourse(String courseName) {
+        return courseDAO.retrieveOneCourse(courseName);
+    }
+
+    public List<Course> retrieveAllCourse() {
+
+        return courseDAO.retrieveAllCourse();
+    }
+
+    public void updateCourse(Course course) {
+
+        courseDAO.updateCourse(course);
+    }
+
+    public void deleteCourse(String courseName) {
+
+        courseDAO.deleteCourse(courseName);
+    }
+}

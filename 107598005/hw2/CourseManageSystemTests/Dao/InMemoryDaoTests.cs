aa9c2cba6436e33cbf205fc1aba4cb67377df9a7
107598005 <t107598005@ntut.org.tw>
2019-04-16 23:29:13 +0800

    hw2

--- 107598005/hw2/CourseManageSystemTests/Dao/InMemoryDaoTests.cs
+++ 107598005/hw2/CourseManageSystemTests/Dao/InMemoryDaoTests.cs
@@ -14,32 +14,78 @@ namespace CourseManageSystem.Dao.Tests
         [TestMethod()]
         public void InMemoryDaoTest()
         {
-
-            Assert.Fail();
+            InMemoryDao Dao = new InMemoryDao();
+            List<CourseDTO> courseDTOs = Dao.ListAllCourse();
+            Assert.AreEqual(0,courseDTOs.Count);
         }
 
         [TestMethod()]
         public void AddCourseTest()
         {
-            Assert.Fail();
+            InMemoryDao Dao = new InMemoryDao();
+            Dao.AddCourse(new CourseDTO("name",
+                                        "des",
+                                        "obj",
+                                        "price",
+                                        "catyion",
+                                        "remarks"));
+            List<CourseDTO> courseDTOs = Dao.ListAllCourse();
+            Assert.AreEqual("name", courseDTOs[0].Name);
         }
 
         [TestMethod()]
         public void ListAllCourseTest()
         {
-            Assert.Fail();
+            InMemoryDao Dao = new InMemoryDao();
+            Dao.AddCourse(new CourseDTO("name",
+                                        "des",
+                                        "obj",
+                                        "price",
+                                        "catyion",
+                                        "remarks"));
+            List<CourseDTO> courseDTOs = Dao.ListAllCourse();
+            Assert.AreEqual(1, courseDTOs.Count);
         }
 
         [TestMethod()]
         public void UpdataCourseTest()
         {
-            Assert.Fail();
+            InMemoryDao Dao = new InMemoryDao();
+            Dao.AddCourse(new CourseDTO("name",
+                                        "des",
+                                        "obj",
+                                        "price",
+                                        "catyion",
+                                        "remarks"));
+            List<CourseDTO> courseDTOs = Dao.ListAllCourse();
+            Assert.AreEqual("name", courseDTOs[0].Name);
+            Dao.UpdataCourse(0, new CourseDTO("Name",
+                                        "123",
+                                        "456",
+                                        "price",
+                                        "catyion",
+                                        "remarks"));
+            courseDTOs = Dao.ListAllCourse();
+            Assert.AreEqual("Name", courseDTOs[0].Name);
+            Assert.AreEqual("123", courseDTOs[0].Description);
+            Assert.AreEqual("456", courseDTOs[0].Objective);
         }
 
         [TestMethod()]
         public void DeleteCourseTest()
         {
-            Assert.Fail();
+            InMemoryDao Dao = new InMemoryDao();
+            Dao.AddCourse(new CourseDTO("name",
+                                        "des",
+                                        "obj",
+                                        "price",
+                                        "catyion",
+                                        "remarks"));
+            List<CourseDTO> courseDTOs = Dao.ListAllCourse();
+            Assert.AreEqual(1, courseDTOs.Count);
+            Dao.DeleteCourse(0);
+            courseDTOs = Dao.ListAllCourse();
+            Assert.AreEqual(0, courseDTOs.Count);
         }
     }
 }
---
+++ 107598005/hw2/CourseManageSystemTests/Dao/XmlFileDaoTests.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CourseManageSystem.Dao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManageSystem.Dao.Tests
+{
+    [TestClass()]
+    public class XmlFileDaoTests
+    {
+        [TestMethod()]
+        public void XmlFileDaoTest()
+        {
+            XmlFileDao Dao = new XmlFileDao("../../coursedata.xml");
+        }
+
+        [TestMethod()]
+        public void AddCourseTest()
+        {
+            XmlFileDao Dao = new XmlFileDao("../../coursedata.xml");
+            List<CourseDTO> courseDTOs = Dao.ListAllCourse();
+            Assert.AreEqual(2, courseDTOs.Count);
+            Dao.AddCourse(new CourseDTO("name",
+                                        "des",
+                                        "obj",
+                                        "price",
+                                        "catyion",
+                                        "remarks"));
+            courseDTOs = Dao.ListAllCourse();
+            Assert.AreEqual(3, courseDTOs.Count);
+            Assert.AreEqual("name", courseDTOs[courseDTOs.Count-1].Name);
+        }
+
+        [TestMethod()]
+        public void ListAllCourseTest()
+        {
+            XmlFileDao Dao = new XmlFileDao("../../coursedata.xml");
+            List<CourseDTO> courseDTOs = Dao.ListAllCourse();
+            Assert.AreEqual(3, courseDTOs.Count);
+        }
+
+        [TestMethod()]
+        public void UpdataCourseTest()
+        {
+            XmlFileDao Dao = new XmlFileDao("../../coursedata.xml");
+            Dao.AddCourse(new CourseDTO("123",
+                                        "des",
+                                        "obj",
+                                        "price",
+                                        "catyion",
+                                        "remarks"));
+            List<CourseDTO> courseDTOs = Dao.ListAllCourse();
+            Assert.AreEqual("123", courseDTOs[courseDTOs.Count - 1].Name);
+            Dao.UpdataCourse(courseDTOs.Count - 1, new CourseDTO("456",
+                                        "des",
+                                        "obj",
+                                        "price",
+                                        "catyion",
+                                        "remarks"));
+            courseDTOs = Dao.ListAllCourse();
+            Assert.AreEqual("456", courseDTOs[courseDTOs.Count - 1].Name);
+            Dao.DeleteCourse(courseDTOs.Count - 1);
+            courseDTOs = Dao.ListAllCourse();
+            Assert.AreEqual(3, courseDTOs.Count);
+        }
+
+        [TestMethod()]
+        public void DeleteCourseTest()
+        {
+            XmlFileDao Dao = new XmlFileDao("../../coursedata.xml");
+            List<CourseDTO> courseDTOs = Dao.ListAllCourse();
+            Dao.DeleteCourse(courseDTOs.Count - 1);
+            courseDTOs = Dao.ListAllCourse();
+            Assert.AreEqual(2, courseDTOs.Count);
+        }
+    }
+}
